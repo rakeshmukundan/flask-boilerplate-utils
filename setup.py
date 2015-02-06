443a3ab21cca5396e5d509ed4b7c7a6fbb4b5d9e
Nick Whyte <nick@nickwhyte.com>
2015-02-06 12:50:13 +1100

    Fix for packaging on linux

--- setup.py
+++ setup.py
@@ -22,7 +22,10 @@ setup(
   description="Additional functionality with easy upgrading for the flask-boilerplate.",
   long_description=long_description,
   url='https://github.com/nickw444/flask-boilerplate-utils',
-  include_package_data=True,
+  # include_package_data=True,
+  # package_data={
+  #   'templates':['*']
+  # },
   zip_safe=False,
   install_requires=[
     "Flask",
